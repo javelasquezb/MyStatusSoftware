1626587af4045a695141de25f7f62508ad3b9679
Javier Velasquez <javelasquezb.d.17@gmail.com>
2021-04-24 01:52:14 -0400

    Subiendo template de pagina web

---
+++ MyStatusSoftware.Data/Entities/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyStatusSoftware.Data.Entities
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+
+        DateTime CreationDate { get; set; }
+
+        int CreationUser { get; set; }
+
+        bool IsEnable { get; set; }
+
+        DateTime UpdateDate { get; set; }
+
+        int UpdateUser { get; set; }
+
+    }
+}
